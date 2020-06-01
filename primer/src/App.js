d416782552942b4a22d0d369d1c988f7e5ad184c
lucas <lucassouzadeveloper@gmail.com>
2020-06-01 11:35:48 +0100

    commited using grid style

--- primer/src/App.js
+++ primer/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { component, Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4,
+    };
+  }
+
+  isEven(val) {
+    return val % 2 === 0 ? "even" : "odd";
+  }
+
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secundary text-primary text-center p-2 m-1";
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1 });
+  render = () => (
+    <div className="container-fluid p-4">
+      <div className="row bg-info text-white p-2">
+        <div className="col font-weight-bold">Value</div>
+        <div className="col-6 font-weight-bold">Even?</div>
+      </div>
+      <div className="row bg-light p-2 border">
+        <div className="col">{this.state.count}</div>
+        <div className="col-6">{this.isEven(this.state.count)}</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-info m-2" onClick={this.handleClick}>
+            Click Me
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
